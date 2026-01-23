038eece03beeba01e21825614ddd22a5520010e6
“Khshanovskyi” <pavlo_khshanovskyi@epam.com>
2026-01-23 19:12:16 +0200

    Initial commit

--- app_demo/d2_interceptors/_interceptor.py
+++ app_demo/d2_interceptors/_interceptor.py
@@ -1,4 +1,4 @@
-from typing import Any
+from time import sleep
 
 from aidial_interceptors_sdk.chat_completion.base import ChatCompletionInterceptor
 from aidial_sdk.chat_completion import Stage
@@ -13,17 +13,20 @@ class PrePostInterceptor(ChatCompletionInterceptor):
     async def on_stream_start(self) -> None:
         with Stage(self.response._queue,
                    0,
-                   0,
+                   self.reserve_stage_index(0),
                    f"{self.interceptor_name} Greeting Stage",
                    ) as stage:
             stage.append_content(f"Hi, from {self.interceptor_name} Greeting Stage👋")
+            sleep(1)
 
     @override
     async def on_stream_end(self) -> None:
+        sleep(1)
         with Stage(self.response._queue,
                    0,
-                   100,
+                   self.reserve_stage_index(0),
                    f"{self.interceptor_name} Goodbye Stage",
                    ) as stage:
             stage.append_content(f"{self.interceptor_name} Goodbye, from {self.interceptor_name} Goodbye Stage👋")
+            sleep(1)
 
